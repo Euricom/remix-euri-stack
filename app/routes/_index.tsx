ad600fe97bdee2cc278017385dc33725155aeabf
Peter Cosemans <peter.cosemans@gmail.com>
2024-09-25 19:00:54 +0200

    Add shadcn with button example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "../components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
@@ -8,6 +9,7 @@ export default function Index() {
   return (
     <div className="h-[144px] w-[434px]">
       <img src="/logo-light.png" alt="Remix" />
+      <Button>My Button</Button>
     </div>
   );
 }
